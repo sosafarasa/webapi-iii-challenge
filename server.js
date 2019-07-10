b964d5909081533d5c6a97515582844d37e909ef
Agustina Sosa <agustinasf85@gmail.com>
2019-07-10 18:25:32 -0400

    Server running and added helmet

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = 'express';
+
+const express = require('express');
+const helmet = require('helmet');
 
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  next()
 };
 
 module.exports = server;
